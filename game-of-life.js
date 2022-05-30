c325fb481ae246e4ddca4d61ca55e7a8d5ab2ad2
Paul <pkilgarriff@gmail.com>
2022-05-30 18:29:10 +0100

    Add nextGeneration function
    - hard-code result for 2x2 dead grid

--- game-of-life.js
+++ game-of-life.js
@@ -2,4 +2,11 @@ const hello = () => {
   return "Hello World";
 };
 
-module.exports = hello;
+const nextGeneration = (currentGeneration) => {
+  return [
+    [0, 0],
+    [0, 0],
+  ];
+};
+
+module.exports = { hello, nextGeneration };
--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -1,7 +1,17 @@
-const hello = require("./game-of-life");
+const { hello, nextGeneration } = require("./game-of-life");
 
-describe("Infrastructure", () => {
+describe("hello", () => {
   test("It says hello world", () => {
     expect(hello()).toEqual("Hello World");
+  });
+});
+
+describe("nextGeneration", () => {
+  test("does nothing to a dead grid", () => {
+    let deadGrid = [
+      [0, 0],
+      [0, 0],
+    ];
+    expect(nextGeneration(deadGrid)).toEqual(deadGrid);
   });
 });
