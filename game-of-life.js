f9b883ebe048dbcafde684389b23616841667e00
Paul <pkilgarriff@gmail.com>
2022-05-30 19:58:14 +0100

    Add listSurroundingCoordinates function

--- game-of-life.js
+++ game-of-life.js
@@ -10,4 +10,25 @@ const nextGeneration = (currentGeneration) => {
   ];
 };
 
-module.exports = { hello, nextGeneration };
+const neitherNegative = (a, b) => {
+  return a >= 0 && b >= 0;
+};
+
+const listSurroundingCoordinates = (matrix, currentX, currentY) => {
+  const surroundingCells = [
+    [currentX - 1, currentY - 1],
+    [currentX - 1, currentY + 0],
+    [currentX - 1, currentY + 1],
+    [currentX + 0, currentY - 1],
+    [currentX + 0, currentY + 1],
+    [currentX + 1, currentY - 1],
+    [currentX + 1, currentY + 0],
+    [currentX + 1, currentY + 1],
+  ];
+  const filteredCells = surroundingCells.filter(([x, y]) =>
+    neitherNegative(x, y)
+  );
+  return filteredCells;
+};
+
+module.exports = { hello, nextGeneration, listSurroundingCoordinates };
--- game-of-life.spec.js
+++ game-of-life.spec.js
@@ -1,4 +1,8 @@
-const { hello, nextGeneration } = require("./game-of-life");
+const {
+  hello,
+  nextGeneration,
+  listSurroundingCoordinates,
+} = require("./game-of-life");
 
 describe("hello", () => {
   test("It says hello world", () => {
@@ -24,5 +28,24 @@ describe("nextGeneration", () => {
       [0, 0],
     ];
     expect(nextGeneration(singleLiveGrid)).toEqual(expected);
+  });
+});
+
+describe("listSurroundingCoordinates", () => {
+  test("Returns array of co-ordinates", () => {
+    let matrix = [
+      [0, 0],
+      [0, 0],
+    ];
+    let expected = [
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ];
+    let currentX = 0;
+    let currentY = 0;
+    expect(listSurroundingCoordinates(matrix, currentX, currentY)).toEqual(
+      expected
+    );
   });
 });
